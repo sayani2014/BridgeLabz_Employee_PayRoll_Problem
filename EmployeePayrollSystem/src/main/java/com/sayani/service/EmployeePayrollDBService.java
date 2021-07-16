528bb61b11f0ff5d2b5096df917a72110d93f002
Sayani Koley <sayanikoley8@gmail.com>
2021-07-16 21:20:52 +0530

    Ability for Employee Payroll Service to retrieve the Employee Payroll from the database

--- EmployeePayrollSystem/src/main/java/com/sayani/service/EmployeePayrollDBService.java
+++ EmployeePayrollSystem/src/main/java/com/sayani/service/EmployeePayrollDBService.java
@@ -17,8 +17,7 @@ public class EmployeePayrollDBService {
     public List<EmployeePayrollData> readData() {
         String sql = "SELECT * FROM employee_payroll";
         List<EmployeePayrollData> employeePayrollList = new ArrayList<>();
-        try {
-            Connection connection = this.getConnection();
+        try (Connection connection = this.getConnection()){
             Statement statement = connection.createStatement();
             ResultSet result = statement.executeQuery(sql);
             while(result.next()) {
